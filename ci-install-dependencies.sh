318847dd70597834143b6f56223a4ec5b76b4c25
Devin Austin <c-devin.austin@charter.com>
2019-05-24 13:35:30 -0600

    attempting to get the build to fail if set up doesn't complete successfully

--- ci-install-dependencies.sh
+++ ci-install-dependencies.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
+set -e
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter &&
 chmod +x ./cc-test-reporter &&
 wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/6.0.0-beta/flyway-commandline-6.0.0-beta-linux-x64.tar.gz | tar xvz && sudo ln -s `pwd`/flyway-6.0.0-beta/flyway /usr/local/bin || true &&
 psql -U postgres < src/main/resources/sql/test-bootstrap.sql
-echo "***************** POSTGRES RUN"
